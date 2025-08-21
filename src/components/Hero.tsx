e492efbbe2c7bf0b8bf8c7a0f349feefedc7f4ef
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2025-08-21 11:31:48 +0300

    update resume url

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -50,7 +50,7 @@ const Hero = () => {
               Get in Touch
             </a>
             <a
-              href="https://drive.google.com/file/d/1ViaD3RSIuN1LFrq7WJsKhDqN--hO2tnk/view?usp=sharing"
+              href="https://drive.google.com/file/d/1l4KtlkM816hwxqPEWaDjvhDFzlqr1rkN/view?usp=sharing"
               target="_blank"
               className="inline-block bg-secondary text-primary font-semibold px-6 py-3 rounded-lg hover:bg-accent transition-colors ml-4"
             >
